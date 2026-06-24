14e36d19acbb149b2cfc9e750dec4b7ba3cab360
Hailey Chen <haileyclay7@gmail.com>
2026-06-24 15:11:43 -0700

    Wrote the analyze image function (created a tester just to make sure hough would work with the pyiamgej platform headless)

--- connection.py
+++ connection.py
@@ -1,35 +1,55 @@
 import imagej, skimage, os, xarray
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
 
-filename = os.path.join(skimage.data_dir, '/Volumes/DISK_IMG/Droplet Picture/IMG_0004.jpg')
-droplets = skimage.io.imread(filename)
-
-xdroplets = xarray.DataArray(droplets, name = 'xdroplets', dims=('t', 'row', 'col'))
 
 # Initialize ImageJ in head mode
 print("Initializing PyImageJ...")
-ij = imagej.init('sc.fiji:fiji')
+ij = imagej.init('/Applications/Fiji')
+#print(f"ImageJ version: {ij.getVersion()}")
+def analyze_image(image_path,  pixel_ratio, unit, num_droplets = 200, threshold = "Default"):
+    macro_args = f"{num_droplets},{threshold},{pixel_ratio},{unit},{image_path}"
+    print(f"what it should be: {macro_args}")
+    macro = open('/Users/haile/Desktop/DmterMeasure.ijm').read()
+    result = ij.py.run_macro(macro,{"argument": macro_args})
+    '''
+    import os
+    print("feret file exists:", os.path.exists('/tmp/feret_values.txt'))
+    print("feret file size:", os.path.getsize('/tmp/feret_values.txt') if os.path.exists('/tmp/feret_values.txt') else "N/A")
+    print("results csv exists:", os.path.exists('/tmp/results.csv'))
+    print('what I got:')
+    print_thing = result.get('printed_args')
+    print(print_thing)'''
+    feret_path = '/tmp/feret_values.txt'
+    with open(feret_path) as f:
+        values = [float(line.strip()) for line in f if line.strip()]
+    
+    fig, ax, = plt.subplots()
+    ax.hist(values, bins = 'auto', edgecolor = 'black')
+    ax.set_xlabel(f"Feret Diameter {unit}")
+    ax.set_ylabel("Count")
+    ax.set_title(f"Droplets Size Distribution(n={len(values)})")
+    plt.tight_layout()
+    plt.savefig("/tmp/histogram.png",dpi = 150)
+    plt.close()
 
-jdroplets = ij.py.to_java(xdroplets)#converting xarray (labeled numpy array) to java
-ij.py.show(jdroplets)
+    return{
+        "histogram": "/tmp/histogram.png",
+        "visual" : "tmp/visual_output.png",
+        "values": values,
+        "n" : len(values),
+        "mean": np.mean(values),
+        "std": np.std(values)
+
+    }
 
 
-# Print the active version to confirm it works
-print(f"ImageJ version: {ij.getVersion()}")
+ 
+print("processing image...")
+analyze_image('/Volumes/DISK_IMG/Droplet Picture/IMG_0003.jpg', 1, "mm",100)
+print("done :)")
 
-#takes in inputs... should ask for num, measurement conversion, (give me pixel to length ratio preferrably?)
-#if num is less than like 120, take in min size and max size estimates... (in pixels). If they don't have, auto should be around 80 - 150
-#once input is ready, calls the appropriate macro with all of the parameters.
-
-numDroplets = int(input("What is a rough estimate of the num of droplets?"))
-measureRatio = float(input("What is the ratio of pixel to unit?"))
-minSize = 80
-maxSize = 140
-
-if numDroplets > 120:
-    #run Macro from Internet
-    print("running macro from internet")
-else:
-    print("running hough circle macro.. will take a minute")
-    #run hough circle macro
-
-
+ij.dispose()
+import jpype
+jpype.shutdownJVM()
